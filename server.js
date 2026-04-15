88dffc579ae19de96234d9e0eb9301c0cf7a45d4
Diya Agarwal <60317798+diya98@users.noreply.github.com>
2026-04-15 14:39:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,12 +20,11 @@ app.use(express.static('public'));
 let db = {};
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: false,
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 });
 
